ba990903a6d4b4d652db14ec67fb31fc37a79b28
rmdai <rmdai@cam2cloud.com>
2015-11-11 13:13:19 +0800

    note

---
+++ note/php.js
@@ -0,0 +1,13 @@
+//array_walk($array,$arrayfunction);
+//
+//class.name.php;
+//
+//    class classA{
+//    function functionA(){}
+//    public parameterA;
+//    private $_privateA;
+//    public function __construct()
+//    {}
+//    public function __destruct(){}
+//
+//}
