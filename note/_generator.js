8edafd5a3a51ed69b0a2c9b89c5b20b9fcf6f14b
rmdai <rmdai@cam2cloud.com>
2018-03-28 21:53:24 +0800

    Add generator js

---
+++ note/_generator.js
@@ -0,0 +1,91 @@
+// iterable对象：js 中的可迭代对象： 必须有 Symbol.iterator 属性
+// [Symbol.iterator] 属性 是 返回一个对象的无参函数，被返回对象符合迭代器协议
+
+// iterator  :An object is an iterator when it implements a next() method with the following semantics:
+// next函数：是一个返回{done:false, value:'' } 形式的无参函数
+
+// generator = iterator + iterable ; because it has a next method, so it's an iterator; because it has an @@iterator method, so it's an iterable
+
+// entries方法: 返回一个可迭代对象的迭代器
+var idMaker = () => {
+  var index = 0
+  return {
+    next: ()=> {return {value: index++, done: false}}
+  }
+}
+
+var iterator
+iterator = idMaker();
+iterator.next();
+iterator.next();
+iterator.next();
+
+iterator = [1, 2, 3, 4, 5, 6][Symbol.iterator]();
+iterator.next();
+iterator.next();
+iterator.next();
+
+iterator = "1234567"[Symbol.iterator]();
+iterator.next();
+iterator.next();
+iterator.next();
+
+// iterable Object
+class simpleClass {
+  [Symbol.iterator]() {
+    return idMaker()
+  }
+}
+var instance = new simpleClass()
+for (var i of instance) {
+  console.log(i)
+  if (i > 15)break
+}
+var generator = function*() {
+  yield 1;
+  yield 2;
+  yield 3;
+}()
+generator.next()
+generator[Symbol.iterator]
+generator[Symbol.iterator]() === generator
+
+function* f() {
+  for (var i = 0; true; i++) {
+    var firstOne = yield i;
+    var another = yield "======== another String ============" + i
+    console.log("The body is executed", "The params is ", firstOne, another)
+    if (firstOne) { i = -1; }
+  }
+}
+var l =(...r)=> console.log('**************')||console.log(...r)
+var g = f();
+
+l(g.next()) //{ value: 0, done: false }
+l(g.next()) //{ value: '======== another String ============0', done: false }
+l(g.next()) //The body is executed The params is  undefined undefined
+            // { value: 1, done: false }
+l(g.next(true))
+// why this send can take affect until the next step?，
+// because generator function's body is executed until the first yield expression
+l(g.next())
+l(g.next())
+l(g.next(true))// why this next(true) wont work?
+l(g.next())
+l(g.next())
+l(g.next())
+g = f();
+l(g.next())
+l(g.next())
+g.next(true) //This params will send to the another not firstOne Why?
+l(g.next())
+l(g.next())
+l(g.next(true))
+l(g.next())
+l(g.next())
+l(g.next())
+
+// Used to send a value to a generator.
+// The value is returned from the yield expression,
+// and returns a value yielded by the next yield expression.
+// send(x) corresponds to next(x) in the ES2015 generator object.
--- note/http.md
+++ note/http.md
@@ -1,8 +1,19 @@
-几乎所有的uri都是url，防止不成立
-所有的对象都有mime 类型
-method 除了curd 还有第3种
-http1.0报文是文本而非二进制
-响应报文分为三个部分： 起始行，首部字段，主体
-状态码：100信息提示，200成功，300重定向，400客户端错误，500 server错误
-网关，隧道，代理的区别是神码
-url 的格式是什么样子？ scheme://user:pwd@host:port/path?params#frag
+* 几乎所有的uri都是url，防止不成立
+* 所有的对象都有mime 类型
+* method 除了curd 还有第3种
+* http1.0报文是文本而非二进制
+* 响应报文分为三个部分： 起始行，首部字段，主体
+* 状态码：100信息提示，200成功，300重定向，400客户端错误，500 server错误
+* 网关，隧道，代理的区别是神码
+* url 的格式是什么样子？ scheme://user:pwd@host:port/path?params#frag
+* dns-prefetch-control  域名缓存meta标志。预处理域名解析，加快速度
+* Strict-Transport-Security: max-age: 15552000; includeSubDomains 告诉浏览器需要用https 来访问这个网站
+* XSS Cross-Site Scripting 
+* csrf Cross-site request forgery
+* DDoS Distributed Denial of Service
+* X-Frame-Options 是否可以存在  iframe 中显示, nginx-demo: add_header X-Frame-Options SAMEORIGIN;
+* X-Content-Type-Options :nosniff 只允许加载 指定的type的 资源
+* X-XSS-Protection 告诉浏览器 xss 的策略,老司机给你一句忠告就是，千万别配置成XSS-Protection: 1?? why
+* multipart/form-data  和 multipart/byteranges 区别
+* hidePoweredBy 去除 X-Powered-By header 可以 保证系统安全
+* etag If-none-match
