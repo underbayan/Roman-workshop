f94be1ffba10c72d8cbe6accaf66ede95d4a0d6c
romanTai <underbayan@gmail.com>
2023-05-28 10:10:00 +0800

    feat: lerna demo

---
+++ demo/lerna/packages/C/index.js
@@ -0,0 +1,4 @@
+const b = require('@theomist/b')
+const a = require('@theomist/a')
+exports.c = "c"
+console.log(b, a)
---
+++ demo/lerna/packages/D/__tests__/D.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const d = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(d(), 'Hello from d');
+console.info('d tests passed');
