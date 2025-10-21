96724ea4f3d164f017992883328105e6c1de34e4
Roman Tai <roman.dai@airwallex.com>
2025-10-21 10:34:26 +0800

    fix: update exe cmd

--- note/js/_exe.git.test.js
+++ note/js/_exe.git.test.js
@@ -1,21 +1,21 @@
-const { exe } = require("./_exe");
+const { run } = require("./_exe");
 const t = async () => {
   try {
-    await exe("git pull");
-    await exe("git --no-pager log");
-    await exe("git checkout  feat/sq");
-    await exe("git add *");
-    await exe('git commit -m "feat: testing interactive git command"');
-    await exe("git push -f");
+    await run("git pull");
+    await run("git --no-pager log");
+    await run("git checkout  feat/sq");
+    await run("git add *");
+    await run('git commit -m "feat: testing interactive git command"');
+    await run("git push -f");
 
     //get latest commit for master
-    console.log(await exe("git rev-parse master"));
+    console.log(await run("git rev-parse master"));
     //get latest commit for current
-    console.log(await exe("git rev-parse HEAD"));
+    console.log(await run("git rev-parse HEAD"));
     // get all branch
-    console.log(await exe("git branch -r"));
+    console.log(await run("git branch -r"));
     // is Master branch
-    console.log((await exe("git rev-parse master")).stdout == (await exe("git rev-parse HEAD")).stdout);
+    console.log((await run("git rev-parse master")).stdout == (await run("git rev-parse HEAD")).stdout);
   } catch (e) {
     console.log(e);
   }
