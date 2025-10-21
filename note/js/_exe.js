d3569331f9938d2d769fc17f4ebedcbd1e65d956
Roman Tai <roman.dai@airwallex.com>
2025-10-21 10:38:23 +0800

    revert exe

--- note/js/_exe.js
+++ note/js/_exe.js
@@ -1,15 +1,24 @@
 const execa = require("execa");
-exports.run = (cmd) => {
-  const parts = [], stack = [];
-  let arg = '';
-  for (let i = 0; i < cmd.length; i++) {
-    const c = cmd[i];
-    if (c === '\\') arg += cmd[++i];
-    else if (['"', "'"].includes(c) && (!stack.length || stack.at(-1) === c))
-      stack.length ? stack.pop() : stack.push(c);
-    else if (c === ' ' && !stack.length) (arg && parts.push(arg), arg = '');
-    else arg += c;
+const cmd = async cmd => await execa(cmd.c, cmd.a);
+const replaceAt = (str, i, text) => str.substr(0, i) + text + str.substr(i + 1);
+/**
+ * @description not supoort multple lines
+ * @param {*} str
+ */
+const formatCommand = str => {
+  let isIn34 = false,
+    isIn39 = false,
+    length = str.length;
+  str = str.trim();
+  for (let i = 0; i < length; i++) {
+    isIn34 = str.charCodeAt(i) === 34 ? !isIn34 : isIn34;
+    isIn39 = str.charCodeAt(i) === 39 ? !isIn39 : isIn39;
+    if (/\s/.test(str[i]) && (isIn34 || isIn39)) {
+      str = replaceAt(str, i, "$_____$");
+      length += 6;
+    }
   }
-  const [cmdName, ...args] = arg ? [...parts, arg] : parts;
-  return execa(cmdName, args);
+  const strArray = str.split(/\s+/).map(i => i.replace(/\$_____\$/g, " "));
+  return { c: strArray[0], a: strArray.slice(1) };
 };
+exports.exe = str => cmd(formatCommand(str));
